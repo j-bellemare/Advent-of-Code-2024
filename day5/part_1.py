b2b0899b038793127698519eb927685bd2420948
j-bellemare <jathanbellemare@gmail.com>
2024-12-05 19:25:45 -0500

    adding day5 part_1

---
+++ day5/part_1.py
@@ -0,0 +1,50 @@
+import re
+
+input_file = open("./inputs/page_r_p.txt", "r")
+
+pattern = r"\b\d{2}\b"
+
+switch = False
+rules = []
+prints = []
+for line in input_file:
+    if line.strip():
+        if switch is False:
+            rules.append(line.strip())
+        if switch is True:
+            prints.append(line.strip())
+    else:
+        switch = True
+
+full_prints = []
+for line in prints:
+    nums = re.findall(pattern, line)
+    for i in range(len(nums)):
+        nums[i] = int(nums[i])
+    full_prints.append(nums)
+
+full_rules = []
+for line in rules:
+    nums = re.findall(pattern, line)
+    for i in range(len(nums)):
+        nums[i] = int(nums[i])
+    full_rules.append(nums)
+
+good_lines = []
+for line in full_prints:
+    good_line = True
+    for num in line:
+        for rule in full_rules:
+            if num == rule[0]:
+                if rule[1] in line:
+                    if line.index(rule[0]) > line.index(rule[1]):
+                        good_line = False
+    if good_line is True:
+        good_lines.append(line)
+
+total = 0
+for line in good_lines:
+    middle = (len(line)-1)/2
+    total += line[int(middle)]
+
+print(total)
