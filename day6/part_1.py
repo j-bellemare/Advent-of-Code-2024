ea33ca57d42eef0c087ca3972c566e501b33d182
j-bellemare <jathanbellemare@gmail.com>
2024-12-06 09:11:51 -0500

    sad

--- day6/part_1.py
+++ day6/part_1.py
@@ -1,4 +1,6 @@
-input_file = open("./inputs/guard_map.txt", "r")
+import time
+
+input_file = open("./inputs/test.txt", "r")
 
 floor_map = []
 i = 0
@@ -10,67 +12,86 @@ for line in input_file:
     i += 1
 
 patrol = True
-while patrol is True:
-    row = 0
-    for line in floor_map:
-        col = 0
-        for char in line:
-            if (
-                (char == "^" and row == 0)
-                or (char == "v" and row == len(floor_map) - 1)
-                or (char == "<" and col == 0)
-                or (char == ">" and col == len(line) - 1)
-            ):
-                patrol = False
+loops = 0
+row1 = 0
+for line1 in floor_map:
+    col1 = 0
+    for char1 in line1:
+        if char1 == ".":
+            floor_map[row1][col1] = "#"
 
-            if char == "^" and row > 0:
-                if floor_map[row - 1][col] != "#":
-                    floor_map[row - 1][col] = "^"
-                elif col + 1 > len(line) - 1:
-                    patrol = False
-                elif floor_map[row][col + 1] != "#":
-                    floor_map[row][col + 1] = ">"
-                else:
-                    floor_map[row - 1][col] = "v"
+        timeout = 60
+        timeout_start = time.time()
+        not_timed_out = True
 
-            elif char == "v" and row < len(floor_map) - 1:
-                if floor_map[row + 1][col] != "#":
-                    floor_map[row + 1][col] = "v"
-                elif col - 1 < 0:
-                    patrol = False
-                elif floor_map[row][col - 1] != "#":
-                    floor_map[row][col - 1] = "<"
-                else:
-                    floor_map[row + 1][col] = "^"
+        while patrol is True and not_timed_out is True:
+            if time.time() > timeout_start + timeout:
+                not_timed_out = False
+                loops += 1
+            row = 0
+            for line in floor_map:
+                col = 0
+                for char in line:
+                    if (
+                        (char == "^" and row == 0)
+                        or (char == "v" and row == len(floor_map) - 1)
+                        or (char == "<" and col == 0)
+                        or (char == ">" and col == len(line) - 1)
+                    ):
+                        patrol = False
 
-            elif char == "<" and col > 0:
-                if floor_map[row][col - 1] != "#":
-                    floor_map[row][col - 1] = "<"
-                elif row - 1 < 0:
-                    patrol = False
-                elif floor_map[row - 1][col] != "#":
-                    floor_map[row - 1][col] = "^"
-                else:
-                    floor_map[row][col + 1] = ">"
+                    if char == "^" and row > 0:
+                        if floor_map[row - 1][col] != "#":
+                            floor_map[row - 1][col] = "^"
+                        elif col + 1 > len(line) - 1:
+                            patrol = False
+                        elif floor_map[row][col + 1] != "#":
+                            floor_map[row][col + 1] = ">"
+                        else:
+                            floor_map[row - 1][col] = "v"
 
-            elif char == ">" and col < len(line) - 1:
-                if floor_map[row][col + 1] != "#":
-                    floor_map[row][col + 1] = ">"
-                elif row + 1 > len(floor_map) - 1:
-                    patrol = False
-                elif floor_map[row + 1][col] != "#":
-                    floor_map[row + 1][col] = "v"
-                else:
-                    floor_map[row][col - 1] = "<"
+                    elif char == "v" and row < len(floor_map) - 1:
+                        if floor_map[row + 1][col] != "#":
+                            floor_map[row + 1][col] = "v"
+                        elif col - 1 < 0:
+                            patrol = False
+                        elif floor_map[row][col - 1] != "#":
+                            floor_map[row][col - 1] = "<"
+                        else:
+                            floor_map[row + 1][col] = "^"
 
-            if char in ["^", "v", ">", "<"]:
-                floor_map[row][col] = "X"
+                    elif char == "<" and col > 0:
+                        if floor_map[row][col - 1] != "#":
+                            floor_map[row][col - 1] = "<"
+                        elif row - 1 < 0:
+                            patrol = False
+                        elif floor_map[row - 1][col] != "#":
+                            floor_map[row - 1][col] = "^"
+                        else:
+                            floor_map[row][col + 1] = ">"
 
-            col += 1
-        row += 1
+                    elif char == ">" and col < len(line) - 1:
+                        if floor_map[row][col + 1] != "#":
+                            floor_map[row][col + 1] = ">"
+                        elif row + 1 > len(floor_map) - 1:
+                            patrol = False
+                        elif floor_map[row + 1][col] != "#":
+                            floor_map[row + 1][col] = "v"
+                        else:
+                            floor_map[row][col - 1] = "<"
 
-# for line in floor_map:
-#     print(line)
+                    if char in ["^", "v", ">", "<"]:
+                        floor_map[row][col] = "X"
+
+                    col += 1
+                row += 1
+            if char1 == ".":
+                floor_map[row1][col1] = "."
+        col1 += 1
+    row1 += 1
+
+for line in floor_map:
+    print(line)
 
 total = 0
 for line in floor_map:
@@ -78,4 +99,4 @@ for line in floor_map:
         if char == "X":
             total += 1
 
-print(total)
+print(loops)
